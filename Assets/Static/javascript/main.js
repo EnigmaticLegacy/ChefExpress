5dd486d71fb60fbdce4ae91a6cd356464880be78
EnigmaticLegacy <goliandialvin@gmail.com>
2022-01-22 00:20:10 +0700

    Update

--- Assets/Static/javascript/main.js
+++ Assets/Static/javascript/main.js
@@ -4,25 +4,4 @@ $(document).ready(function() {
         $("#body").addClass('loaded');
     }, 3000);
 
-    var owl = $('.owl-carousel');
-        owl.owlCarousel({
-            margin: 10,
-            loop:true,
-            autoplay:true,
-            autoplayTimeout:5000,
-            autoplayHoverPause:true,
-            dots:false,
-            responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 5
-            }
-            }
-    });
-
 });
